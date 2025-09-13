62628fc30dfb256e324cd49b6a95b9e9bc546de8
christian tshibangu <christshims2015@gmail.com>
2025-09-13 18:24:47 +0200

    adding of simple interest fonction

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+print(f'The simple interest for a principal amount of {principal}, rate of {rate}%, over {time} years is {interest}.')
