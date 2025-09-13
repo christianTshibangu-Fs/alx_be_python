e1058d6d8326cf11a4f03f262f573c2524ffc1b3
christian tshibangu <christshims2015@gmail.com>
2025-09-13 18:12:53 +0200

    Update of basic Operations fonction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1=10
+number2=5
+
+result=number1+number2
+print(f'The sum of {number1} and {number2} is {result}')
+
+result=number1-number2
+print(f'The difference of {number1} and {number2} is {result}')
+
+result=number1*number2
+print(f'The product of {number1} and {number2} is {result}')
+
+result=number1/number2
+print(f'The division of {number1} by {number2} is {result}')
