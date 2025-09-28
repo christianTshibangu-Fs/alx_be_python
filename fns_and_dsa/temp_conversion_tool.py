d72895d5bf61d6fc7bdd1c21d3e7c1854e9bbf9b
christian tshibangu <christshims2015@gmail.com>
2025-09-29 00:04:57 +0200

    adding temp conversion feature

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,29 @@
+
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5.0 / 9.0
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9.0 / 5.0
+
+
+def convert_to_celsius(fahrenheit):
+
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+
+
+def convert_to_fahrenheit(celsius):
+
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return fahrenheit
+
+
+
+temperature = float(input("Entrez la température: ")) 
+unit = input("Convertir en (F)ahrenheit ou (C)elsius? ").strip().upper()
+if unit == 'F' :
+    convert_to_celsius = convert_to_celsius(temperature)
+    print(f"{temperature}°C est égal à {convert_to_celsius}°F")
+elif unit == 'C' :
+    convert_to_fahrenheit = convert_to_fahrenheit(temperature)
+    print(f"{temperature}°F est égal à {convert_to_fahrenheit}°C")
+else :
+    print("Unité non reconnue. Veuillez entrer 'F' ou 'C'.")
+
